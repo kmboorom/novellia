1b94f00ba88fa75ebb7ab2a84065dfb27a727431
kmboo <kmboorom@gmail.com>
2025-02-26 11:54:17 -0500

    oh look the basics work all of a sudden

--- load_data.sql
+++ load_data.sql
@@ -10,26 +10,4 @@ COPY observation (data)
 FROM '/tmp/data/Observation.ndjson'  
 WITH (FORMAT text);
 
-CREATE TEMP TABLE temp_patient (data text);
-
--- Load the file using COPY in TEXT format
-COPY temp_patient (data) 
-FROM '/tmp/data/Patient.ndjson' 
-WITH (FORMAT text);
-
--- Now insert the data into the actual patient table after converting to jsonb
-DO $$ 
-DECLARE
-    rec text;
-BEGIN
-    FOR rec IN SELECT data FROM temp_patient LOOP
-        BEGIN
-            -- Try to insert as JSONB into the patient table
-            INSERT INTO patient (data) VALUES (rec::jsonb);
-        EXCEPTION
-            WHEN OTHERS THEN
-                -- Skip any invalid data and raise a notice
-                RAISE NOTICE 'Skipping invalid line: %', rec;
-        END;
-    END LOOP;
-END $$;
+copy public.patient FROM '/tmp/data/Patient.ndjson' csv quote e'\x01' delimiter e'\x02';
